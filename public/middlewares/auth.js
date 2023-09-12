6f80d6db147704318a94bb459dc61ecfa062f6f0
tonghoang <hoangtd241100@gmail.com>
2023-09-13 00:46:08 +0700

    update

--- public/middlewares/auth.js
+++
@@ -1,6 +0,0 @@
-const LogAccess = ({ context, info }, next) => {
-    const username = context.username || "guest";
-    console.log(`Logging access: ${username} -> ${info.parentType.name}.${info.fieldName}`);
-    return next();
-};
-//# sourceMappingURL=auth.js.map
